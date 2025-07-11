df0e1be63315afac984ef9dedfcbbb83a2367dc5
Lanqing Yuan <yuanlq@uchicago.edu>
2025-07-10 19:07:58 -0500

    Debug data_kind

--- straxion/plugins/raw_records.py
+++ straxion/plugins/raw_records.py
@@ -84,13 +84,13 @@ class DAQReader(strax.Plugin):
 
     # Data structure topology related:
     provides: str = "raw_records"
-    data_kind = immutabledict(zip(provides, provides))
+    data_kind = provides
     depends_on: Tuple = tuple()  # This is the lowest level of strax data in processing.
 
     # Memory management related:
     rechunk_on_load = False  # Assumed no single dataset will be larger than 1 GB.
     chunk_source_size_mb = strax.DEFAULT_CHUNK_SIZE_MB  # 200 MB; Neglected if no rechunk on load.
-    rechunk_on_save = immutabledict(raw_records=False)  # Assumed no chunking at DAQ.
+    rechunk_on_save = False  # Assumed no chunking at DAQ.
     chunk_target_size_mb = 1000  # Meaningless if rechunk_on_save is False.
     compressor = "lz4"  # Inherited from straxen. Not optimized outside XENONnT.
 
