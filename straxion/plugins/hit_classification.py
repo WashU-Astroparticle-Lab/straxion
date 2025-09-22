11d8a03f630ba19d866e750ca124009ac42e052d
Lanqing Yuan <yuanlq@uchicago.edu>
2025-09-22 02:37:57 -0500

    Enhance hit classification with dynamic spike thresholds
    Added configurable options for spike threshold calculation using signal statistics, including per-channel sigma and noise multipliers. Implemented logic to dynamically determine spike thresholds and identify symmetric spike hits. Updated hit classification to exclude symmetric spikes from photon candidates and refactored related methods for improved flexibility and accuracy.

--- straxion/plugins/hit_classification.py
+++ straxion/plugins/hit_classification.py
@@ -27,10 +27,31 @@ export, __all__ = strax.exporter()
     ),
     strax.Option(
         "spike_threshold_dx",
-        default=0.6e-6,
+        default=None,
         track=True,
         type=float,
         help="Threshold for spike finding in units of dx=df/f0.",
+    ),
+    strax.Option(
+        "spike_thresholds_sigma",
+        default=[3.0 for _ in range(41)],
+        track=True,
+        type=list,
+        help=(
+            "Threshold for spike finding in units of sigma of standard deviation of the noise. "
+            "If None, the spike threshold will be calculated based on the signal statistics."
+        ),
+    ),
+    strax.Option(
+        "noisy_channel_signal_std_multipliers",
+        default=[2.0 for _ in range(41)],
+        track=True,
+        type=list,
+        help=(
+            "If the signal standard deviation above this threshold times of signal absolute "
+            "mean, the signal is considered noisy and the spike threshold is increased. "
+            "If None, the spike threshold will be calculated based on the signal statistics."
+        ),
     ),
     strax.Option(
         "fs",
@@ -48,11 +69,20 @@ export, __all__ = strax.exporter()
             "in unit of samples."
         ),
     ),
+    strax.Option(
+        "symmetric_spike_min_slope",
+        type=list,
+        default=[0.0 for _ in range(41)],
+        help=(
+            "Minimum rise edge slope of the moving averaged signal for identifying a physical hit "
+            "against symmetric spikes, in unit of dx/second."
+        ),
+    ),
 )
 class SpikeCoincidence(strax.Plugin):
     """Classify hits into different types based on their coincidence with spikes."""
 
-    __version__ = "0.0.0"
+    __version__ = "0.1.0"
 
     depends_on = ("hits", "records")
     provides = "hit_classification"
@@ -68,6 +98,7 @@ class SpikeCoincidence(strax.Plugin):
 
         hit_id_dtype = [
             (("Is in coincidence with spikes", "is_coincident_with_spikes"), bool),
+            (("Is symmetric spike hit", "is_symmetric_spike"), bool),
             (("Photon candidate hit", "is_photon_candidate"), bool),
         ]
 
@@ -92,9 +123,94 @@ class SpikeCoincidence(strax.Plugin):
             round(self.config["spike_coincidence_window"] * self.config["fs"])
         )
         self.spike_threshold_dx = self.config["spike_threshold_dx"]
+        self.ss_min_slope = self.config["symmetric_spike_min_slope"]
         self.ss_window = self.config["symmetric_spike_inspection_window_length"]
         self.max_spike_coincidence = self.config["max_spike_coincidence"]
         self.dt_exact = 1 / self.config["fs"] * SECOND_TO_NANOSECOND
+
+    @staticmethod
+    def calculate_spike_threshold(
+        signal, spike_threshold_sigma, noisy_channel_signal_std_multiplier
+    ):
+        """Calculate hit threshold based on signal statistics.
+        The algorithm is the same as DxHits.calculate_hit_threshold.
+
+        Args:
+            signal (np.ndarray): The signal array to analyze.
+            hit_threshold_sigma (float): Threshold multiplier in units of sigma.
+            noisy_channel_signal_std_multiplier (float): Multiplier to detect noisy channels.
+
+        Returns:
+            float: The calculated hit threshold.
+
+        """
+        signal_mean = np.mean(signal, axis=1)
+        signal_abs_mean = np.mean(np.abs(signal), axis=1)
+        signal_std = np.std(signal, axis=1)
+
+        # The naive hit threshold is a multiple of the standard deviation of the signal.
+        spike_threshold = signal_mean + spike_threshold_sigma * signal_std
+
+        # If the signal is noisy, the baseline might be too high.
+        for ch in range(len(signal_std)):
+            if signal_std[ch] > noisy_channel_signal_std_multiplier[ch] * signal_abs_mean[ch]:
+                # We will use the quiet part of the signal to redefine a lowered hit threshold.
+                quiet_mask = signal[ch] < spike_threshold[ch]
+                spike_threshold[ch] = signal_mean[ch] + spike_threshold_sigma[ch] * np.std(
+                    signal[ch][quiet_mask]
+                )
+
+        return spike_threshold
+
+    def determine_spike_threshold(self, records):
+        """Determine the spike threshold based on the provided configuration.
+        You can either provide spike_threshold_dx or
+        (hit_thresholds_sigma and noisy_channel_signal_std_multipliers).
+        You cannot provide both.
+        The algorithm is the same as DxHits.determine_hit_threshold.
+        """
+        if (
+            self.spike_threshold_dx is None
+            and self.spike_thresholds_sigma is not None
+            and self.noisy_channel_signal_std_multipliers is not None
+        ):
+            # If hit_thresholds_sigma and noisy_channel_signal_std_multipliers are single values,
+            # we need to convert them to arrays.
+            if isinstance(self.spike_thresholds_sigma, float):
+                self.spike_thresholds_sigma = np.full(
+                    len(records["channel"]), self.spike_thresholds_sigma
+                )
+            else:
+                self.spike_thresholds_sigma = np.array(self.spike_thresholds_sigma)
+            if isinstance(self.noisy_channel_signal_std_multipliers, float):
+                self.noisy_channel_signal_std_multipliers = np.full(
+                    len(records["channel"]), self.noisy_channel_signal_std_multipliers
+                )
+            else:
+                self.noisy_channel_signal_std_multipliers = np.array(
+                    self.noisy_channel_signal_std_multipliers
+                )
+            # Calculate spike threshold and find spike candidates
+            self.spike_threshold_dx = self.calculate_spike_threshold(
+                records["data_dx_convolved"],
+                self.spike_thresholds_sigma[records["channel"]],
+                self.noisy_channel_signal_std_multipliers[records["channel"]],
+            )
+        elif (
+            self.spike_threshold_dx is not None
+            and self.spike_thresholds_sigma is None
+            and self.noisy_channel_signal_std_multipliers is None
+        ):
+            # If spike_threshold_dx is a single value, we need to convert it to an array.
+            if isinstance(self.spike_threshold_dx, float):
+                self.spike_threshold_dx = np.full(len(records["channel"]), self.spike_threshold_dx)
+            else:
+                self.spike_threshold_dx = np.array(self.spike_threshold_dx)
+        else:
+            raise ValueError(
+                "Either spike_threshold_dx or spike_thresholds_sigma and "
+                "noisy_channel_signal_std_multipliers must be provided. You cannot provide both."
+            )
 
     def compute_rise_edge_slope(self, hits, hit_classification):
         """Compute the rise edge slope of the moving averaged signal."""
@@ -103,21 +219,27 @@ class SpikeCoincidence(strax.Plugin):
         dt = self.dt_exact
         times = np.arange(self.ss_window) * dt / SECOND_TO_NANOSECOND
 
-        inspected_wfs = hits["data_dx"][
+        inspected_wfs = hits["data_dx_moving_average"][
             :, HIT_WINDOW_LENGTH_LEFT - self.ss_window : HIT_WINDOW_LENGTH_LEFT
         ]
         # Fit a linear model to the inspected window.
         hit_classification["rise_edge_slope"] = np.polyfit(times, inspected_wfs.T, 1)[0]
 
+    def is_symmetric_spike_hit(self, hits, hit_classification):
+        """Identify symmetric spike hits."""
+        hit_classification["is_symmetric_spike"] = (
+            hit_classification["rise_edge_slope"] < self.ss_min_slope[hits["channel"]]
+        )
+
     def find_spike_coincidence(self, hit_classification, hits, records):
-        """Find the spike coincidence of the hit."""
+        """Find the spike coincidence of the hit in the convolved signal."""
         spike_coincidence = np.zeros(len(hits))
         for i, hit in enumerate(hits):
             # Get the index of the hit maximum in the record
             hit_climax_i = hit["amplitude_max_record_i"]
 
             # Extract windows from all records at once
-            inspected_wfs = records["data_dx"][
+            inspected_wfs = records["data_dx_convolved"][
                 :,
                 hit_climax_i
                 - self.spike_coincidence_window : hit_climax_i
@@ -125,10 +247,14 @@ class SpikeCoincidence(strax.Plugin):
             ]
 
             # Count records with spikes above threshold
-            spike_coincidence[i] = np.sum(np.max(inspected_wfs, axis=1) > self.spike_threshold_dx)
+            spike_coincidence[i] = np.sum(
+                np.max(inspected_wfs, axis=1) > self.spike_threshold_dx[records["channel"]]
+            )
         hit_classification["n_spikes_coinciding"] = spike_coincidence
 
     def compute(self, hits, records):
+        self.determine_spike_threshold(records)
+
         hit_classification = np.zeros(len(hits), dtype=self.infer_dtype())
         hit_classification["time"] = hits["time"]
         hit_classification["endtime"] = hits["endtime"]
@@ -140,7 +266,10 @@ class SpikeCoincidence(strax.Plugin):
         hit_classification["is_coincident_with_spikes"] = (
             hit_classification["n_spikes_coinciding"] > self.max_spike_coincidence
         )
-        hit_classification["is_photon_candidate"] = ~hit_classification["is_coincident_with_spikes"]
+        hit_classification["is_photon_candidate"] = ~(
+            hit_classification["is_coincident_with_spikes"]
+            | hit_classification["is_symmetric_spike"]
+        )
 
         return hit_classification
 
