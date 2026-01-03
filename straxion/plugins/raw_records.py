Fix chunk end time calculation in NX3LikeReader
Replaces use of self.dt with self.dt_exact when calculating the chunk end time to ensure more accurate timing based on the exact data type.

straxion/plugins/raw_records.py
@@ -552,7 +552,7 @@ class NX3LikeReader(strax.Plugin):
         # We must build a chunk for the lowest data type, as required by strax.
         results = self.chunk(
             start=np.min(results["time"]),
-            end=np.max(results["time"]) + np.int64(self.dt * self.config["record_length"]),
+            end=np.max(results["time"]) + np.int64(self.dt_exact * self.config["record_length"]),
             data=results,
             data_type="raw_records",
         )